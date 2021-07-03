0364aa2cf808ed163f451ace2b9af86e606fe827
Marcel Cozono <marcel.cozono@gmail.com>
2021-07-03 19:03:50 -0300

    Modulo 13

--- src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,15 +1,61 @@
 package com.algaworks.algafood.infrastructure.service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
+
+import org.springframework.stereotype.Repository;
+
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
 
+@Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
 
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@Override
-	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
-		return null;
+	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
+		var builder = manager.getCriteriaBuilder();
+		var query = builder.createQuery(VendaDiaria.class);
+		var root = query.from(Pedido.class);
+		var predicates = new ArrayList<Predicate>();
+		
+		var functionConvertTZDataCriacao = builder.function("convert_tz", Date.class, root.get("dataCriacao"), builder.literal("+00:00"), builder.literal(timeOffset));
+		
+		var functionDateDataCriacao = builder.function("date", Date.class, functionConvertTZDataCriacao);
+		
+		var selection = builder.construct(VendaDiaria.class, 
+						functionDateDataCriacao, 
+						builder.count(root.get("id")),
+						builder.sum(root.get("valorTotal")));
+		
+		if (filtro.getDataCriacaoInicio() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+		}
+		
+		if (filtro.getDataCriacaoFim() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+		}
+		
+		if (filtro.getRestauranteId() != null) {
+			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+		}
+		
+		predicates.add(root.get("status").in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+		
+		query.select(selection);
+		query.where(predicates.toArray(new Predicate[] {}));
+		query.groupBy(functionDateDataCriacao);
+		
+		return manager.createQuery(query).getResultList();
 	}
 }
